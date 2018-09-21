3795675597fd8d489b16d089f3e9557e0120602b
Wilmata <wilmermata.24@gmail.com>
2018-09-20 21:48:07 -0600

    Objects

---
+++ src/java/Domain/EntitySets.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Wilmata
+ */
+public class EntitySets {
+    
+    private String name;
+    private String type;
+    private String parentEntitySet;
+    private LinkedList attributes;
+
+    @Override
+    public String toString() {
+        return "EntitySets{" + "name=" + name + ", type=" + type + ", parentEntitySet=" + parentEntitySet + ", attributes=" + attributes + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getParentEntitySet() {
+        return parentEntitySet;
+    }
+
+    public void setParentEntitySet(String parentEntitySet) {
+        this.parentEntitySet = parentEntitySet;
+    }
+
+    public LinkedList getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(LinkedList attributes) {
+        this.attributes = attributes;
+    }
+}
